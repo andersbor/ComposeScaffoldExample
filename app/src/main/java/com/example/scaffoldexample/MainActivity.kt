200d158831da6ba0bffaebe5a71307bbd052e95f
andersbor <anbo@zealand.dk>
2024-08-08 12:07:40 +0200

    more modifiers

--- app/src/main/java/com/example/scaffoldexample/MainActivity.kt
+++ app/src/main/java/com/example/scaffoldexample/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -31,7 +34,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.scaffoldexample.ui.theme.ScaffoldExampleTheme
@@ -93,7 +99,15 @@ fun ScaffoldExample() {
                     You have pressed the floating action button $presses times.
                 """.trimIndent(),
             )
-            Text(text = "More content")
+            Text(
+                text = "More content",
+                modifier = Modifier
+                    .background(Color.Yellow)
+                    .padding(20.dp)
+                    //.fillMaxWidth() // not good with CenterHorizontally
+                    .align(CenterHorizontally)
+                    .clickable { presses++ }
+            )
         }
     }
 }
@@ -140,7 +154,7 @@ fun MyButtonBar() {
 }
 
 
-@Preview(showBackground = true)
+@Preview()
 @Composable
 fun ScaffoldPreview() {
     ScaffoldExampleTheme {
